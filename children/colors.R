447812a2416d0f9e8096dabd970717d8d05a03b9
Sebastian Sauer <ssauer@posteo.de>
2024-03-01 15:19:47 +0100

    add cols

---
+++ children/colors.R
@@ -0,0 +1,17 @@
+
+$$\definecolor{ycol}{RGB}{230,159,0}
+\definecolor{modelcol}{RGB}{86,180,233}
+\definecolor{errorcol}{RGB}{0,158,115}
+\definecolor{beta0col}{RGB}{213,94,0}
+\definecolor{beta1col}{RGB}{0,114,178}
+\definecolor{xcol}{RGB}{204,121,167}$$
+  
+
+ycol <- "#E69F00"
+modelcol <- "#56B4E9"
+errorcol <- "#009E73"
+beta0col <- "#D55E00"
+beta1col <- "#0072B2"
+xcol <- "#CC79A7"
+
+

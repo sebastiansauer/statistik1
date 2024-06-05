f3fe67c96d7409a1fcfb194c71a187318a858d5e
Sebastian Sauer <ssauer@posteo.de>
2024-06-05 22:45:33 +0200

    update regression1

--- children/colors.R
+++ children/colors.R
@@ -1,11 +1,11 @@
 
-$$\definecolor{ycol}{RGB}{230,159,0}
-\definecolor{modelcol}{RGB}{86,180,233}
-\definecolor{errorcol}{RGB}{0,158,115}
-\definecolor{beta0col}{RGB}{213,94,0}
-\definecolor{beta1col}{RGB}{0,114,178}
-\definecolor{xcol}{RGB}{204,121,167}$$
-  
+# $$\definecolor{ycol}{RGB}{230,159,0}
+# \definecolor{modelcol}{RGB}{86,180,233}
+# \definecolor{errorcol}{RGB}{0,158,115}
+# \definecolor{beta0col}{RGB}{213,94,0}
+# \definecolor{beta1col}{RGB}{0,114,178}
+# \definecolor{xcol}{RGB}{204,121,167}$$
+#   
 
 ycol <- "#E69F00"
 modelcol <- "#56B4E9"
